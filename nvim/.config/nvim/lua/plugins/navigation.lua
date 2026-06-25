9eb442112aeca409681513b12caba304d0a7ea4b
Benjamin Ye <benjaminye.email@gmail.com>
2026-06-24 19:33:46 -0500

    fix telescope

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -1,7 +1,6 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
         dependencies = {
             "nvim-lua/plenary.nvim",
             {
@@ -17,9 +16,6 @@ return {
             vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
             vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
             vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-            -- vim.keymap.set("n", "<leader>sg", function()
-            --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            -- end, { desc = "[S]earch by [G]rep" })
             vim.keymap.set("n", "<leader>sn", function()
                 builtin.find_files({ cwd = vim.fn.stdpath("config") })
             end, { desc = "[S]earch [N]eovim Config" })
